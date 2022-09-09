057ee5a7fa17252a24826bac64a1a864d4c6c59a
Amin <medamineharwel@outlook.com>
2022-09-09 15:29:54 +0100

    added an auth guard

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from './auth/auth.service';
 import { RequestService } from './http/request.service';
 import { PersistenceService, STORAGE } from './services/persistence.service';
+import { AuthGuard } from './guards/auth.guard';
 
 
 
@@ -15,6 +16,7 @@ import { PersistenceService, STORAGE } from './services/persistence.service';
     AuthService,
     RequestService,
     PersistenceService,
+    AuthGuard
     // { provide: STORAGE, useFactory: () => sessionStorage}
   ]
 })
